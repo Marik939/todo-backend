f373472c07e850d591110adf076d019fbba983ba
marik <mmandryk25@students.oamk.fi>
2026-03-23 14:33:51 +0200

    add routes

---
+++ routes/todo.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+let tasks = [
+  { id: 1, description: "Test task" }
+];
+
+// получить все задачи
+router.get('/', (req, res) => {
+  res.json(tasks);
+});
+
+// добавить задачу
+router.post('/new', (req, res) => {
+  const { description } = req.body;
+  const newTask = {
+    id: Date.now(),
+    description
+  };
+  tasks.push(newTask);
+  res.json(newTask);
+});
+
+// удалить задачу
+router.delete('/delete/:id', (req, res) => {
+  const { id } = req.params;
+  tasks = tasks.filter(t => t.id != id);
+  res.json({ id });
+});
+
+module.exports = { todoRouter: router };
